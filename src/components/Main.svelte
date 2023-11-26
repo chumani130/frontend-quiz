b710f5fc2121482474c5f56ba9d01450db549743
chumani <chumanimadikizela@gmail.com>
2023-11-26 11:58:49 +0200

    initial svelte commit

---
+++ src/components/Main.svelte
@@ -0,0 +1,22 @@
+<div class="w-1/3 mx-auto mt-20 bg-white rounded-lg overflow-hidden shadow-md text-center">
+    <div class="flex justify-between items-center bg-blue-500 text-white p-4">
+      <h1 class="text-2xl font-bold m-0">Celo Wisdom Tokens</h1>
+      <button class="text-3xl text-gray-400" >
+        <i data-feather="plus-circle"></i>
+      </button>
+    </div>
+    <div class="h-full w-1 bg-gray-300"></div>
+    <div class="p-6">
+      <button class="w-full py-4 text-lg rounded-md bg-blue-500 text-white cursor-pointer mb-4">Start a Quiz</button>
+      <p class="text-lg text-black mb-4">
+        Welcome to Celo Wisdom Tokens! Embark on a journey through the wonders of quizzes and knowledge. Earn delightful rewards, and unlock even more treasures when you share the experience with your friends.
+    </p>
+    </div>
+    <div class="border border-gray-300 rounded-md p-4 text-left mt-4">
+      <h2 class="text-lg text-black font-bold mb-2">Take the Quiz</h2>
+      <p class="text-black ">Really awesome quiz to talk about the experience of Celo Blockchain</p>
+    </div>
+    <button class="bg-blue-500 text-white rounded-md py-2 px-4 cursor-pointer mt-4">
+        Invite Friends
+    </button>
+  </div>
